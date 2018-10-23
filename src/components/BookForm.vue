8996ed213987129785013cf96257543838bce05b
Jordan Barber <wjbarber3@gmail.com>
2018-10-23 14:09:36 -0600

    completing module

---
+++ src/components/BookForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form class="" action="#" method="post" v-on:submit="bookSubmit(bookTitle, bookAuthor)">
+    <input type="text" name="title" value="" placeholder="Book Title" v-model="bookTitle">
+    <input type="text" name="author" value="" placeholder="Book Author" v-model="bookAuthor">
+    <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'BookForm',
+    props: [ 'books' ],
+    data() {
+      return {
+        bookTitle,
+        bookAuthor
+      }
+    },
+    methods: {
+      bookSubmit(bookTitle, bookAuthor) {
+        this.$emit('addBook', bookTitle, bookAuthor);
+      }
+    }
+  }
+</script>
+
+<style></style>
